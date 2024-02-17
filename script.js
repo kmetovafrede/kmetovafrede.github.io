76c28a6bbd222309b11d02b8e249753fb7436df9
whoislisalisa <xkmeto00@vutbr.cz>
2024-02-17 15:50:40 +0100

    hiding menu added

---
+++ script.js
@@ -0,0 +1,24 @@
+// Frederika Kmeťová
+// ITW druhý projekt
+// 2023/2024
+// .js file
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuContent = document.querySelector('.menu_content');
+    menuContent.querySelectorAll('a').forEach(link => {
+        link.addEventListener('click', () => {
+            menuContent.style.right = '-200px';
+        });
+    });
+    
+    const menuButton = document.querySelector('.menu_button');
+    menuButton.addEventListener('click', () => {
+        if (menuContent.style.right === '0px') {
+            // If menu is open, close it
+            menuContent.style.right = '-200px';
+        } else {
+            // If menu is closed, open it
+            menuContent.style.right = '0px';
+        }
+    });
+});
