2d738048580d4858a0a07a9d13ea826031518dc7
whoislisalisa <xkmeto00@vutbr.cz>
2024-02-18 21:35:39 +0100

    loader added

--- script.js
+++ script.js
@@ -29,3 +29,39 @@ function flipCard(card) {
 function toggleDropdown(card) {
     card.classList.toggle('card-flipped');
 }
+document.addEventListener("DOMContentLoaded", function() {
+    // Show the loaders
+    var loaders = document.querySelectorAll(".loader, .coffee-loader");
+    loaders.forEach(function(loader) {
+        loader.style.display = "block";
+    });
+
+    // Hide the entire page content, including background image and footer initially
+    document.querySelector("body").style.backgroundImage = "none";
+    var container = document.querySelector(".container");
+    container.style.display = "none";
+    container.style.opacity = "0"; 
+    var footer = document.querySelector(".footer");
+    footer.style.display = "none";
+    footer.style.opacity = "0"; 
+
+    // Hide the loaders and smoothly reveal the webpage content after 2 seconds (to match coffee loader animation duration)
+    setTimeout(function() {
+        loaders.forEach(function(loader) {
+            loader.style.display = "none"; // Hide the loaders
+        });
+
+        container.style.display = "block";
+        footer.style.display = "block";
+        document.querySelector("body").style.backgroundImage = "url('img/background.jpg')"; // Show the background image
+
+        // Trigger CSS animation to fade in the content
+        container.style.animation = "fadeIn 1s ease-in-out"; // Smooth transition with ease-in-out timing function
+        container.style.opacity = "1"; // Set opacity to 1 after animation
+        footer.style.animation = "fadeIn 1s ease-in-out"; 
+        footer.style.opacity = "1"; 
+        body.style.animation = "fadeIn 1s ease-in-out"; 
+        body.style.opacity = "1"; 
+    }, 3800); // TODO 3800 seconds are top
+});
+
