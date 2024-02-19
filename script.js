e0aee50a05e7a8fefc0a9897df85a027ee936be0
whoislisalisa <xkmeto00@vutbr.cz>
2024-02-19 22:30:45 +0100

    loader added

--- script.js
+++ script.js
@@ -82,6 +82,6 @@ document.addEventListener("DOMContentLoaded", function() {
         footer.style.opacity = "1"; 
         document.body.style.animation = "fadeIn 1s ease-in-out"; 
         document.body.style.opacity = "1"; 
-    }, 1); // TODO 3800 seconds are top
+    }, 3800); // TODO 3800 seconds are top
 });
 
