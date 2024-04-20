4b790d6fbda480c51312291ecc2e02244d0d5f59
whoislisalisa <xkmeto00@vutbr.cz>
2024-04-20 19:45:00 +0200

    comments half done and small changes

--- carousel.js
+++ carousel.js
@@ -3,20 +3,27 @@
 // 2023/2024
 // .js file
 
+// slideshow for carousel going through images 
 document.addEventListener("DOMContentLoaded", function() {
     const slideshow = document.querySelector('.slideshow');
+    //debug if slideshow is not found
     if (!slideshow) {
         console.error('Slideshow element not found.');
         return;
     }
     
+    // getting all the slides
     const slides = document.querySelectorAll('.slideshow input[type="radio"]');
     let slideIndex = 0;
 
+    // showing the next slide
     function showNextSlide() {
         slides[slideIndex].checked = false;
         slideIndex = (slideIndex + 1) % slides.length;
         slides[slideIndex].checked = true;
     }
+    // setting the interval for 5 seconds
     setInterval(showNextSlide, 5000);
 });
+
+// end of carousel.js ----------------------------------------------------------------------------
--- script.js
+++ script.js
@@ -3,6 +3,7 @@
 // 2023/2024
 // .js file
 
+// toggle the visibility of the menu
 function toggleMenuVisibility() {
     var menuContent = document.querySelector('.menu_content');
     var menuIcon = document.querySelector('.menu');
@@ -10,6 +11,7 @@ function toggleMenuVisibility() {
     var menuRightValue = menuContent.style.right;
     if (menuRightValue === '' || menuRightValue === '-200px') {
         menuContent.style.right = '0';
+        // open both icon and menu
         menuContent.classList.add('opened'); 
         menuIcon.classList.add('opened'); 
     } else {
@@ -17,18 +19,22 @@ function toggleMenuVisibility() {
     }
 }
 
+// closing menu
 function closeMenu() {
     var menuContent = document.querySelector('.menu_content');
     var menuIcon = document.querySelector('.menu');
+    // closing, by setting the right property
     menuContent.style.right = '-200px';
     menuContent.classList.remove('opened'); 
     menuIcon.classList.remove('opened'); 
 }
 
+// event listener for clicks outside the menu
 document.addEventListener('click', function(event) {
     var menuContent = document.querySelector('.menu_content');
     var menuIcon = document.querySelector('.menu');
 
+    // checking where click happened
     var isClickInsideMenu = menuIcon.contains(event.target) || menuContent.contains(event.target);
 
     if (!isClickInsideMenu) {
@@ -36,27 +42,30 @@ document.addEventListener('click', function(event) {
     }
 });
 
+// event listener for opening menu
 var menuIcon = document.querySelector('.menu');
 menuIcon.addEventListener('click', toggleMenuVisibility);
 
-// Add event listener to the menu content to close the menu when clicked
+// event listener for closing menu when clicked
 var menuContent = document.querySelector('.menu_content');
 menuContent.addEventListener('click', function(event) {
     closeMenu();
-    event.stopPropagation(); // Prevent the event from bubbling up and closing the menu immediately
+    event.stopPropagation(); // prevent the event from closing the menu
 });
 
-// Close the menu when the window is scrolled
+// event listener for closing menu when scrolling
 window.addEventListener('scroll', function() {
     if (menuContent.classList.contains('opened')) {
         closeMenu();
     }
 });
 
+// flipping cards
 function flipCard(card) {
     card.classList.toggle("card-flipped");
 }
 
+// showing content behind flipped cards
 function toggleDropdown(card, event) {
     var dropdownContent = card.querySelector('.dropdown-content');
     if (dropdownContent && !dropdownContent.contains(event.target)) {
@@ -64,116 +73,122 @@ function toggleDropdown(card, event) {
     }
 }
 
+// loading of the page
 document.addEventListener("DOMContentLoaded", function() {
-    var loaders = document.querySelectorAll(".loader, .coffee-loader");
-    loaders.forEach(function(loader) {
-        loader.style.display = "block";
-    });
+  // display loaders
+  var loaders = document.querySelectorAll(".loader, .coffee-loader");
+  loaders.forEach(function(loader) {
+      loader.style.display = "block";
+  });
 
-    document.querySelector("body").style.backgroundImage = "none";
-    var container = document.querySelector(".container");
-    container.style.display = "none";
-    container.style.opacity = "0"; 
-    var footer = document.querySelector(".footer");
-    footer.style.display = "none";
-    footer.style.opacity = "0"; 
+  // hide the main website
+  document.querySelector("body").style.backgroundImage = "none";
+  var container = document.querySelector(".container");
+  container.style.display = "none";
+  container.style.opacity = "0"; 
+  var footer = document.querySelector(".footer");
+  footer.style.display = "none";
+  footer.style.opacity = "0"; 
 
-    setTimeout(function() {
-        loaders.forEach(function(loader) {
-            loader.style.display = "none"; 
-        });
+  // show the main website after 2.8 seconds
+  setTimeout(function() {
+      loaders.forEach(function(loader) {
+          loader.style.display = "none"; 
+      });
 
-        container.style.display = "block";
-        footer.style.display = "block";
-        document.querySelector("body").style.backgroundImage = "url('img/satin_wallpaper2.jpg')"; 
+      container.style.display = "block";
+      footer.style.display = "block";
+      document.querySelector("body").style.backgroundImage = "url('img/satin_wallpaper2.jpg')"; 
 
-        container.style.animation = "fadeIn 1s ease-in-out"; 
-        container.style.opacity = "1"; 
-        footer.style.animation = "fadeIn 1s ease-in-out"; 
-        footer.style.opacity = "1"; 
-        document.body.style.animation = "fadeIn 1s ease-in-out"; 
-        document.body.style.opacity = "1"; 
-    }, 2800); // TODO 2800 seconds are top
+      container.style.animation = "fadeIn 1s ease-in-out"; 
+      container.style.opacity = "1"; 
+      footer.style.animation = "fadeIn 1s ease-in-out"; 
+      footer.style.opacity = "1"; 
+      document.body.style.animation = "fadeIn 1s ease-in-out"; 
+      document.body.style.opacity = "1"; 
+  }, 2800); // delay for 2.8 seconds 
 });
 
+// end of script.js ----------------------------------------------------------------------------
 
+
+// Part of my website (portfolio) not the project itself, that i want to re-implement again after the end of semester
 // Back to top button found online by CodyHouse
-// Usage: codyhouse.co/license
-(function() {
-    var backTop = document.getElementsByClassName('js-back-to-top')[0];
-    if( backTop ) {
-      var dataElement = backTop.getAttribute('data-element');
-      var scrollElement = dataElement ? document.querySelector(dataElement) : window;
-      var scrollOffsetInit = parseInt(backTop.getAttribute('data-offset-in')) || parseInt(backTop.getAttribute('data-offset')) || 0, //show back-to-top if scrolling > scrollOffset
-        scrollOffsetOutInit = parseInt(backTop.getAttribute('data-offset-out')) || 0, 
-        scrollOffset = 0,
-        scrollOffsetOut = 0,
-        scrolling = false;
+// (function() {
+//     var backTop = document.getElementsByClassName('js-back-to-top')[0];
+//     if( backTop ) {
+//       var dataElement = backTop.getAttribute('data-element');
+//       var scrollElement = dataElement ? document.querySelector(dataElement) : window;
+//       var scrollOffsetInit = parseInt(backTop.getAttribute('data-offset-in')) || parseInt(backTop.getAttribute('data-offset')) || 0, //show back-to-top if scrolling > scrollOffset
+//         scrollOffsetOutInit = parseInt(backTop.getAttribute('data-offset-out')) || 0, 
+//         scrollOffset = 0,
+//         scrollOffsetOut = 0,
+//         scrolling = false;
   
-      var targetIn = backTop.getAttribute('data-target-in') ? document.querySelector(backTop.getAttribute('data-target-in')) : false,
-        targetOut = backTop.getAttribute('data-target-out') ? document.querySelector(backTop.getAttribute('data-target-out')) : false;
+//       var targetIn = backTop.getAttribute('data-target-in') ? document.querySelector(backTop.getAttribute('data-target-in')) : false,
+//         targetOut = backTop.getAttribute('data-target-out') ? document.querySelector(backTop.getAttribute('data-target-out')) : false;
   
-      updateOffsets();
+//       updateOffsets();
       
-      backTop.addEventListener('click', function(event) {
-        event.preventDefault();
-        if(!window.requestAnimationFrame) {
-          scrollElement.scrollTo(0, 0);
-        } else {
-          dataElement ? scrollElement.scrollTo({top: 0, behavior: 'smooth'}) : window.scrollTo({top: 0, behavior: 'smooth'});
-        } 
-        moveFocus(document.getElementById(backTop.getAttribute('href').replace('#', '')));
-      });
+//       backTop.addEventListener('click', function(event) {
+//         event.preventDefault();
+//         if(!window.requestAnimationFrame) {
+//           scrollElement.scrollTo(0, 0);
+//         } else {
+//           dataElement ? scrollElement.scrollTo({top: 0, behavior: 'smooth'}) : window.scrollTo({top: 0, behavior: 'smooth'});
+//         } 
+//         moveFocus(document.getElementById(backTop.getAttribute('href').replace('#', '')));
+//       });
       
-      checkBackToTop();
-      if (scrollOffset > 0 || scrollOffsetOut > 0) {
-        scrollElement.addEventListener("scroll", function(event) {
-          if( !scrolling ) {
-            scrolling = true;
-            (!window.requestAnimationFrame) ? setTimeout(function(){checkBackToTop();}, 250) : window.requestAnimationFrame(checkBackToTop);
-          }
-        });
-      }
+//       checkBackToTop();
+//       if (scrollOffset > 0 || scrollOffsetOut > 0) {
+//         scrollElement.addEventListener("scroll", function(event) {
+//           if( !scrolling ) {
+//             scrolling = true;
+//             (!window.requestAnimationFrame) ? setTimeout(function(){checkBackToTop();}, 250) : window.requestAnimationFrame(checkBackToTop);
+//           }
+//         });
+//       }
   
-      function checkBackToTop() {
-        updateOffsets();
-        var windowTop = scrollElement.scrollTop || document.documentElement.scrollTop;
-        if(!dataElement) windowTop = window.scrollY || document.documentElement.scrollTop;
-        var condition =  windowTop >= scrollOffset;
-        if(scrollOffsetOut > 0) {
-          condition = (windowTop >= scrollOffset) && (window.innerHeight + windowTop < scrollOffsetOut);
-        }
-        backTop.classList.toggle('back-to-top--is-visible', condition);
-        scrolling = false;
-      }
+//       function checkBackToTop() {
+//         updateOffsets();
+//         var windowTop = scrollElement.scrollTop || document.documentElement.scrollTop;
+//         if(!dataElement) windowTop = window.scrollY || document.documentElement.scrollTop;
+//         var condition =  windowTop >= scrollOffset;
+//         if(scrollOffsetOut > 0) {
+//           condition = (windowTop >= scrollOffset) && (window.innerHeight + windowTop < scrollOffsetOut);
+//         }
+//         backTop.classList.toggle('back-to-top--is-visible', condition);
+//         scrolling = false;
+//       }
   
-      function updateOffsets() {
-        scrollOffset = getOffset(targetIn, scrollOffsetInit, true);
-        scrollOffsetOut = getOffset(targetOut, scrollOffsetOutInit);
-      }
+//       function updateOffsets() {
+//         scrollOffset = getOffset(targetIn, scrollOffsetInit, true);
+//         scrollOffsetOut = getOffset(targetOut, scrollOffsetOutInit);
+//       }
   
-      function getOffset(target, startOffset, bool) {
-        var offset = 0;
-        if(target) {
-          var windowTop = scrollElement.scrollTop || document.documentElement.scrollTop;
-          if(!dataElement) windowTop = window.scrollY || document.documentElement.scrollTop;
-          var boundingClientRect = target.getBoundingClientRect();
-          offset = bool ? boundingClientRect.bottom : boundingClientRect.top;
-          offset = offset + windowTop;
-        }
-        if(startOffset && startOffset) {
-          offset = offset + parseInt(startOffset);
-        }
-        return offset;
-      }
+//       function getOffset(target, startOffset, bool) {
+//         var offset = 0;
+//         if(target) {
+//           var windowTop = scrollElement.scrollTop || document.documentElement.scrollTop;
+//           if(!dataElement) windowTop = window.scrollY || document.documentElement.scrollTop;
+//           var boundingClientRect = target.getBoundingClientRect();
+//           offset = bool ? boundingClientRect.bottom : boundingClientRect.top;
+//           offset = offset + windowTop;
+//         }
+//         if(startOffset && startOffset) {
+//           offset = offset + parseInt(startOffset);
+//         }
+//         return offset;
+//       }
   
-      function moveFocus(element) {
-        if( !element ) element = document.getElementsByTagName("body")[0];
-        element.focus();
-        if (document.activeElement !== element) {
-          element.setAttribute('tabindex','-1');
-          element.focus();
-        }
-      };
-    }
-  }());
+//       function moveFocus(element) {
+//         if( !element ) element = document.getElementsByTagName("body")[0];
+//         element.focus();
+//         if (document.activeElement !== element) {
+//           element.setAttribute('tabindex','-1');
+//           element.focus();
+//         }
+//       };
+//     }
+//   }());
